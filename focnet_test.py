72516c13aa964165a9d71124a02b8608c3930797
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-06-11 11:18:01 +0200

    added seed to unit test

--- focnet_test.py
+++ focnet_test.py
@@ -4,6 +4,8 @@ import tensorflow as tf
 from focnet import FocNet
 
 
+tf.random.set_seed(0)
+
 def test_focnet():
     model = FocNet(n_filters=4)
     model.build(tf.TensorShape([None, None, None, 1]))
