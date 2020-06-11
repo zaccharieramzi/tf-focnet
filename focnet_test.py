72760cba0dcd81d9c9b27836759d141d9307b71c
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-06-11 10:47:36 +0200

    added test to check that all variables change

--- focnet_test.py
+++ focnet_test.py
@@ -6,3 +6,15 @@ from focnet import FocNet
 def test_focnet():
     model = FocNet(n_filters=4)
     model.build(tf.TensorShape([None, None, None, 1]))
+
+def test_focnet_change():
+    model = FocNet(n_filters=4)
+    x = tf.random.normal((1, 64, 64, 1))
+    y = x
+    model(x)
+    before = [v.numpy() for v in model.trainable_variables]
+    model.compile(optimizer='sgd', loss='mse')
+    model.train_on_batch(x, y)
+    after = [v.numpy() for v in model.trainable_variables]
+    for b, a in zip(before, after):
+        assert np.any(np.not_equal(b, a))
